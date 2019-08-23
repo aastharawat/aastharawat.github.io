3757675b9215adb85b6bfacc7f3ccf230d85c7f6
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2019-08-23 20:57:42 +0530

    😎 New Design

---
+++ js/resume.js
@@ -0,0 +1,49 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#sideNav"
+  });
+})(jQuery); // End of use strict
+
+// Using JS for Color WORK
+// const root = document.documentElement;
+
+// function randomNumber(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
+
+// root.addEventListener("mousemove", e => {
+//   root.style.setProperty(
+//     "--primary-color",
+//     `hsl(${e.offsetX}, ${e.offsetY}%, 25%)`
+//   );
+// });
